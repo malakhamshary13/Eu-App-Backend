df620105a9813ccb2e132dab5c0bac5174b227b1
Karim-Mohamed-Elsayed <karim2282004@gmail.com>
2026-04-23 08:51:06 +0200

    fix : cors and middleware

--- main.py
+++ main.py
@@ -2,10 +2,18 @@ from fastapi import FastAPI
 from db.database import Base, engine
 from modules.users.router import router as auth_router
 from modules.users import models
+from fastapi.middleware.cors import CORSMiddleware
+app = FastAPI(title="EU App Backend API")
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 Base.metadata.create_all(bind=engine)
 
-app = FastAPI(title="EU App Backend API")
 
 app.include_router(auth_router)
 
